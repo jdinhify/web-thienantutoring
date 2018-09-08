d105e364c510ff130e60c1351d0df7bd921e562a
jdinhify <jdinhify@gmail.com>
2018-09-08 18:14:10 +1000

    🔨  init

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    semi: [
+      2,
+      'never'
+    ],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        'extensions': [
+          '.js',
+          '.jsx'
+        ]
+      }
+    ],
+  }
+}
